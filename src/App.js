61ab547e8e056b76694c3abf47dcb7638637cff0
IdaliaGpe <idaliaguadalupepucca@hotmail.com>
2020-09-15 11:02:14 -0700

    tacos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-import PageTitle from './components/PageTitle';
+import TacosAdmin from './components/TacosAdmin';
 
 export default class App  extends Component{
   render(){
     return(
-      <PageTitle text='Taco Js' color='purple'/>
+      <TacosAdmin/>
     );
   }
 }
--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,21 +1,27 @@
 import React, {Component, Fragment} from 'react';
 
-export default class App  extends Component{
+export default class PageTitle extends Component{
+
+    state = {
+        fontSize: this.props.fontSize ? this.props.fontSize : 14
+    }; 
 
     componentDidUpdate(){
         
     }
 
-    render(){
-      return(
-       <Fragment>
-            <input type = "number" name = "" id = "" value = '0'/>
+    render() {
+        return (
+            <Fragment>
+                <input type="number" name="" id="" onChange={event =>{
+                    this.setState({fontSize: event.target.value});
+                }}/>
 
-            <h1 style = {{
-            color: this.props.color,
-            fontSize: '24em',
-        }}>{this.props.text}</h1>
-       </Fragment>
-      );
+                <h1 style={{
+                    color: this.props.color,
+                    fontSize: `${this.state.fontSize}em`
+                }}>{this.props.text}</h1>
+            </Fragment>
+        );
     }
-  }
+}
